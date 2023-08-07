719cfb1c614e7361874b6f5788435251bd8ea31c
Hyunmin Cho <hcho2@c1r5s7.42seoul.kr>
2023-08-07 14:02:59 +0900

    [fix] : Fix ft_getenvnode to handle keys with or without value.

--- src/ft_unset.c
+++ src/ft_unset.c
@@ -3,15 +3,24 @@
 t_list	*ft_getenvnode(t_list **environ, char *word)
 {
 	t_list	*iter;
+	char	*new_word;
 
 	iter = *environ;
 	while (iter)
 	{
-		if (!ft_strncmp(iter->content, word, ft_strlen(word)))
-			break ;
+		if (!ft_strcmp(iter->content, word))
+			return (iter);
 		iter = iter->next;
 	}
-	return (iter);
+	new_word = ft_strjoin(word, "=");
+	iter = *environ;
+	while (iter)
+	{
+		if (!ft_strncmp(iter->content, word, ft_strlen(word)))
+			return (iter);
+		iter = iter->next;
+	}
+	return (NULL);
 }
 
 void	ft_unset(char **simple_cmd, t_list **environ, int fd)
@@ -20,8 +29,8 @@ void	ft_unset(char **simple_cmd, t_list **environ, int fd)
 	int		i;
 
 	(void)fd;
-	tmp = NULL;
 	i = 0;
+	tmp = NULL;
 	error_status = 0;
 	while (simple_cmd[++i])
 	{
@@ -30,5 +39,3 @@ void	ft_unset(char **simple_cmd, t_list **environ, int fd)
 			ft_lstdel_mid(environ, tmp);
 	}
 }
-
-
